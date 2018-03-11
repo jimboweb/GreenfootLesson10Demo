d70628864a6cc72da3fb4398cbea0405c958c614
jimboweb <jimboweb@gmail.com>
2018-03-11 15:58:39 -0400

    finished lesson

--- GreenfootLesson10Finished/Player.java
+++ GreenfootLesson10Finished/Player.java
@@ -9,12 +9,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Player extends Actor
 {
     int speed = 2;
+    int length;
     class Direction {
         public static final int UP = 270;
         public static final int DOWN = 90;
         public static final int LEFT = 180;
         public static final int RIGHT = 0;
     }
+    
+    public Player(){
+        length = getImage().getWidth();
+    }
+    
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -35,16 +41,21 @@ public class Player extends Actor
             movePlayer();
         }
     }    
-    
+
     public void movePlayer(){
         int currentX = getX();
         int currentY = getY();
         int direction = getRotation();
         int changeX = getChangeX(direction);
         int changeY = getChangeY(direction);
-        setLocation(currentX + changeX,currentY + changeY);
+        int adjustedChangeX = adjustOffset(changeX);
+        int adjustedChangeY = adjustOffset(changeY);
+        Actor obstacle = getOneObjectAtOffset(adjustedChangeX, adjustedChangeY, Obstacle.class);
+        if(obstacle==null){
+            setLocation(currentX + changeX,currentY + changeY);
+        }
     }
-    
+
     private int getChangeX(int direction){
         if(direction == Direction.RIGHT){
             return speed;
@@ -54,7 +65,7 @@ public class Player extends Actor
         }
         return 0;
     }
-    
+
     private int getChangeY(int direction){
         if(direction == Direction.DOWN){
             return speed;
@@ -64,5 +75,11 @@ public class Player extends Actor
         }
         return 0;
     }
-    
+
+    private int adjustOffset(int offset){
+        int signOfOffset = (int)Math.signum(offset);
+        int distanceToFront = length/2;
+        int adjustAmount = distanceToFront * signOfOffset;
+        return offset + adjustAmount;
+    }
 }
